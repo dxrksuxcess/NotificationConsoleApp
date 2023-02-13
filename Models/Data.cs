57e118d433581ac2a4b89a88dbaf26c2f917b916
dxrksuxcess <dxrksuxcess@gmail.com>
2023-02-13 13:43:26 +0700

    Initial commit

---
+++ Models/Data.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace NotificationApp.Models
+{
+    public class DataBase
+    {
+        public string? SenderEmail { get; set; }
+        public string? SenderName { get; set; }
+        public string? PasswordOfSenderEmail { get; set; }
+        public string? MailRuServerSmtp { get; set; }
+        public int DefPortSmtp { get; set; }
+        public int ReservePortSmtp { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using NotificationApp.Models;
+using NotificationApp.Service;
+using System;
+using System.Net;
+using System.Net.Mail;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json.Serialization;
+
+namespace NotificationApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*var data = new DataBase   -   Создание .json файла
+            {
+                SenderEmail = "testemail_elcomplus@mail.ru",
+                NameSunder = "Ilya Fedin",
+                PasswordOfSenderEmail = "rxvQPERfYSNe2NNFqjAv"
+            };
+
+            string jsonObject = JsonConvert.SerializeObject(data, Formatting.Indented);
+            Console.WriteLine(jsonObject);
+            File.WriteAllText("test.json", jsonObject);*/
+
+
+            Console.WriteLine("Enter the subject of message: ");
+            string? subject = Console.ReadLine();
+
+            Console.WriteLine("Enter the text of message: ");
+            string? body = Console.ReadLine();
+
+            Console.WriteLine("Enter the recipient's email name");
+            string? recipient;
+            //Проверка на пустое значение почты получателя
+            do
+            {
+                recipient = Console.ReadLine();
+                if (string.IsNullOrEmpty(recipient))
+                {
+                    Console.Write("Recipient's email name = null\n");
+                }
+            } while (string.IsNullOrEmpty(recipient));
+
+            // Отправка сообщения на почту
+            SendEmail.FuncSendEmail(subject!, body!, recipient!);
+
+       /* => new Program().MainAsync().GetAwaiter().GetResult();*/
+        }
+    }
+
+}
