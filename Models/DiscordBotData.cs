c1e151c976ca961e3721488f6f98c8ef64d1295c
dxrksuxcess <dxrksuxcess@gmail.com>
2023-03-02 12:48:23 +0700

    Edit class DiscordBotData

---
+++ Models/DiscordBotData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotificationApp.Models
+{
+    public class DiscordBotData
+    {
+        public string? userJira { get; set; }
+        public ulong? userDiscord { get; set; }
+    }
+}
